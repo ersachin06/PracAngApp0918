a1ca8f9e84b2cad2738e55a8f91406913f7ac981
ersachin06 <er.sachin06@gmail.com>
2018-09-18 11:02:10 +0530

    temp11 1101

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { C3DirectiveComponent } from './c3-directive/c3-directive.component';
 import { UserstempComponent } from './userstemp/userstemp.component';
 import { Userstemp11Component } from './userstemp11/userstemp11.component';
 import { Userstemp12Component } from './userstemp12/userstemp12.component';
+import { Temp11Component } from './temp11/temp11.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +18,8 @@ import { Userstemp12Component } from './userstemp12/userstemp12.component';
     C3DirectiveComponent,
     UserstempComponent,
     Userstemp11Component,
-    Userstemp12Component
+    Userstemp12Component,
+    Temp11Component
   ],
   imports: [
     BrowserModule,
